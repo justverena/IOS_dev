c6a215f919b6b9a2c9e11609795df3149234c627
justverena <gaazeverena@gmail.com>
2025-09-19 09:41:52 +0500

    Initial Commit

---
+++ IOS_dev/IOS_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_devApp.swift
+//  IOS_dev
+//
+//  Created by Verena Gaaze on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS_devTests/IOS_devTests.swift
@@ -0,0 +1,16 @@
+//
+//  IOS_devTests.swift
+//  IOS_devTests
+//
+//  Created by Verena Gaaze on 19.09.2025.
+//
+
+import Testing
+
+struct IOS_devTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
